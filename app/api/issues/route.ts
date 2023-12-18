cabf42ed3bf385714459ca40a8c5013e14574989
Antti Jankeri <antti.jankeri@gmail.com>
2023-12-18 10:56:10 +0100

    feat: working database connection + GET issues

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,3 +1,13 @@
-export const GET = async () => {};
+import { NextResponse } from "next/server";
+import prisma from "../../client";
+
+export const GET = async (req: Request) => {
+  try {
+    const data = await prisma.issue.findMany();
+    return NextResponse.json(data);
+  } catch (error) {
+    return NextResponse.json(error, { status: 500 });
+  }
+};
 
 export const POST = async () => {};
