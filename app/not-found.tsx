766aaed8d013e47de700346e25bc8f5894c14f88
Antti Jankeri <antti.jankeri@gmail.com>
2023-12-20 16:15:51 +0100

    feat: 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,12 @@
+const NotFound = () => {
+  return (
+    <>
+      <h1 className="next-error-h1">404</h1>
+      <div>
+        <h2>This page could not be found.</h2>
+      </div>
+    </>
+  );
+};
+
+export default NotFound;
