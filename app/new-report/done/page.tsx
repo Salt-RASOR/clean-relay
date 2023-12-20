dd308346e8098284882314512148d9e8864736f4
Svitlana <rsv0011@gmail.com>
2023-12-20 17:07:41 +0100

    feat: Submitted Report Page layout

--- app/new-report/done/page.tsx
+++ app/new-report/done/page.tsx
@@ -1,12 +1,17 @@
 "use client";
-
 import React from "react";
-import "react-tabs/style/react-tabs.css";
+import Image from "next/image";
+import submittedReport from "@/app/public/submitted_report.svg";
 
 const Page = () => {
   return (
     <div className="background-container bg-violet px-2">
-      <span>Thank you for reporting! Svitlana please style me!</span>
+      <div className="flex flex-col justify-center items-center pt-20">
+        <Image width={200} height={200} src={submittedReport} alt="" />
+        <p className="font-bold text-center text-primary_color text-2xl max-w-80">
+          Thank you! <br /> Your report has been submitted
+        </p>
+      </div>
     </div>
   );
 };
