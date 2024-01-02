32c83731b60ca5077ef5bfe7188da1395c38cdae
Svitlana <rsv0011@gmail.com>
2024-01-02 14:42:34 +0100

    feat: style the Not Found Page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,11 +1,44 @@
+"use client";
+import clsx from "clsx";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import notConnected from "@/app/public/notConnected.svg";
+import Button from "./components/Buttons/Button";
+
 const NotFound = () => {
+  const router = useRouter();
+
+  const redirectToHome = () => {
+    router.push("/");
+  };
   return (
-    <>
-      <h1 className="next-error-h1">404</h1>
-      <div>
-        <h2>This page could not be found.</h2>
+    <div
+      className={clsx(
+        "px-4",
+        "flex flex-col justify-center items-center",
+        "background-container bg-violet",
+        "text-primary_color"
+      )}>
+      <div
+        className={clsx(
+          "w-full max-w-xl",
+          "flex flex-col justify-center items-center"
+        )}>
+        <h1 className="font-bold text-center text-5xl">
+          Oops...
+        </h1>
+        <Image 
+        className="my-4"
+        src={notConnected} alt={"page not found"} width={200} height={200} />
+        <h2 className="font-bold text-2xl">Page Not Found</h2>
+        <p className="mt-3 mb-20 text-center">We can’t find the page you are looking for</p>
+        <Button
+          buttonText={"Go to home page"}
+          additionalClasses={"w-full"}
+          clickHandler={redirectToHome}
+        />
       </div>
-    </>
+    </div>
   );
 };
 
