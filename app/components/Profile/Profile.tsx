cb11b05caecc3b89a681231703add5419b213546
Svitlana <rsv0011@gmail.com>
2024-01-02 10:19:12 +0100

    feat: new Profile component style

--- app/components/Profile/Profile.tsx
+++ app/components/Profile/Profile.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React from "react";
+import clsx from "clsx";
 import Button from "../Buttons/Button";
 import CustomInput from "../Input/CustomInput";
 
@@ -11,6 +12,7 @@ type ProfileProps = {
   handleSignOut: () => void;
   handleUpdateSubmit: (event: React.SyntheticEvent) => void;
 };
+
 const Profile: React.FC<ProfileProps> = ({
   nameRef,
   emailRef,
@@ -22,49 +24,52 @@ const Profile: React.FC<ProfileProps> = ({
   // ToDo get default values from API
   const currentName = "Bob Marley";
   const currentEmail = "Bob@gmail.com";
-
   return (
-    <div className="w-full md:w-7/12 m-auto">
-      <div
-        className="px-4 py-20 background-container bg-violet "
-        style={{ height: "200px" }}>
-        <div className="flex items-center justify-between">
-          <div className="text-primary_color">Hello, <span>{currentName}</span> </div>
-          <Button
-            buttonText={"Sign Out"}
-            clickHandler={handleSignOut}
-            additionalClasses="w-[6.5rem]"
-          />
-        </div>
+    <div
+      className={clsx(
+        "px-4 py-20",
+        "background-container bg-violet",
+        "md:w-7/12 m-auto "
+      )}>
+      <div className="w-full flex items-center justify-between mb-40">
+        <div className="text-primary_color">Hello, user</div>
+        <Button
+          buttonText={"Sign Out"}
+          clickHandler={handleSignOut}
+          additionalClasses="w-[6.5rem]"
+        />
       </div>
-      <div className="px-4 mt-14">
-        <h1 className="font-bold mb-10 text-primary_color text-center text-lg">
-          Update Profile
-        </h1>
-        <form className="w-full" onSubmit={handleUpdateSubmit}>
-          <CustomInput
-            label={"Name"}
-            inputType={"text"} 
-            defaultValue={currentName}
-            forwardedRef={nameRef}
-            hasError={errors}
-          />
-          <CustomInput
-            label={"New Email"}
-            inputType={"email"}
-            defaultValue={currentEmail}
-            forwardedRef={emailRef}
-            hasError={errors}
-          />
-          <CustomInput
-            label={"New Password"}
-            inputType={"password"}
-            forwardedRef={passwordRef}
-            hasError={errors}
-          />
-          <Button buttonText={"Update Profile"} additionalClasses={"my-6 w-full"} />
-        </form>
-      </div>
+    
+      <h1 className="font-bold mb-10 text-primary_color text-center text-lg">
+        Update Profile
+      </h1>
+      <form className="w-full" onSubmit={handleUpdateSubmit}>
+        <CustomInput
+          label={"Name"}
+          inputType={"text"}
+          defaultValue={currentName}
+          forwardedRef={nameRef}
+          hasError={errors}
+        />
+        <CustomInput
+          label={"New Email"}
+          inputType={"email"}
+          defaultValue={currentEmail}
+          forwardedRef={emailRef}
+          hasError={errors}
+        />
+        <CustomInput
+          label={"New Password"}
+          inputType={"password"}
+          forwardedRef={passwordRef}
+          hasError={errors}
+        />
+        <Button
+          buttonText={"Update Profile"}
+          additionalClasses={"my-6 w-full"}
+        />
+      </form>
+ 
     </div>
   );
 };
