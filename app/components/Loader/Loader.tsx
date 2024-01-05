a186945803f96f0d994dbb666b23e18e21483086
Antti Jankeri <antti.jankeri@gmail.com>
2024-01-05 11:35:52 +0100

    feat: loader up higher

--- app/components/Loader/Loader.tsx
+++ app/components/Loader/Loader.tsx
@@ -1,15 +1,17 @@
 import { FadeLoader } from "react-spinners";
-import clsx from'clsx'
+import clsx from "clsx";
 
 const Loader = () => {
   return (
-    <div className={clsx(
-        'fixed top-0 left-0',
-        'w-full h-full',
-        'z-20  bg-bg_overlay '
-      )}>
+    <div
+      className={clsx(
+        "fixed top-0 left-0",
+        "w-full h-full",
+        "z-100  bg-bg_overlay "
+      )}
+    >
       <div className="absolute top-1/2 right-1/2 transform translate-x-1/2 -translate-y-1/2">
-        <FadeLoader color="#f7ecff"/>
+        <FadeLoader color="#f7ecff" />
       </div>
     </div>
   );
