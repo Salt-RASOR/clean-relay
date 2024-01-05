1fb65ad465f9f7a1ed91af2f6d8d667128345092
Antti Jankeri <antti.jankeri@gmail.com>
2024-01-05 12:01:34 +0100

    feat: profile page styling

--- app/components/Profile/Profile.tsx
+++ app/components/Profile/Profile.tsx
@@ -37,61 +37,65 @@ const Profile: React.FC<ProfileProps> = ({
   const { handleModalShow, showModal, handleModalClose } = useHandleModal();
 
   return (
-    <div className={clsx("px-6 py-16", "background-container bg-violet")}>
-      <div className="max-w-2xl m-auto">
-        <h1 className="font-bold mb-12 text-primary_color text-center text-lg">
-          Update Profile
-        </h1>
-        <div className="flex flex-col-reverse sm:flex-row gap-12 items-center justify-between mb-6">
-          <div className="flex items-center">
-            <input
-              id="helper-checkbox"
-              type="checkbox"
-              checked={checked}
-              className="w-5 h-5 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2"
-              onChange={handleChange}
-            />
-            <label
-              htmlFor="helper-checkbox"
-              className="font-medium text-gray-900 dark:text-gray-300 ml-4 text-md"
-            >
-              I want to fix issues
-            </label>
-          </div>
-          <div className="flex items-center justify-right gap-4">
-            <Button
-              buttonText={"Log Out"}
-              clickHandler={handleSignOut}
-              additionalClasses="w-[6.5rem]"
-            />
-            <Button
-              buttonText={"Delete"}
-              clickHandler={handleModalShow}
-              additionalClasses="w-[6.5rem] bg-toast-red"
-            />
-          </div>
-        </div>
-        <form onSubmit={handleUpdateSubmit}>
-          <CustomInput
-            label={"Name"}
-            inputType={"text"}
-            defaultValue={currentUserName}
-            forwardedRef={nameRef}
-            hasError={false}
+    <div
+      className={clsx(
+        "px-4",
+        "flex flex-col justify-center items-center",
+        "background-container bg-violet"
+      )}
+    >
+      <h1 className="font-bold mb-12 text-primary_color text-center text-lg">
+        Update Profile
+      </h1>
+      <div className="flex flex-col-reverse sm:flex-row gap-12 items-center justify-between mb-6 w-full max-w-xl">
+        <div className="flex items-center">
+          <input
+            id="helper-checkbox"
+            type="checkbox"
+            checked={checked}
+            className="w-5 h-5 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2"
+            onChange={handleChange}
           />
-          <CustomInput
-            label={"Phone"}
-            inputType={"number"}
-            defaultValue={currentUserPhone}
-            forwardedRef={phoneRef}
-            hasError={false}
+          <label
+            htmlFor="helper-checkbox"
+            className="font-medium text-gray-900 dark:text-gray-300 ml-4 text-md"
+          >
+            I want to fix issues
+          </label>
+        </div>
+        <div className="flex items-center justify-right gap-4">
+          <Button
+            buttonText={"Log Out"}
+            clickHandler={handleSignOut}
+            additionalClasses="w-[6.5rem]"
           />
           <Button
-            buttonText={"Update Profile"}
-            additionalClasses={"my-6 w-full"}
+            buttonText={"Delete"}
+            clickHandler={handleModalShow}
+            additionalClasses="w-[6.5rem] bg-toast-red"
           />
-        </form>
+        </div>
       </div>
+      <form onSubmit={handleUpdateSubmit} className="w-full max-w-xl">
+        <CustomInput
+          label={"Name"}
+          inputType={"text"}
+          defaultValue={currentUserName}
+          forwardedRef={nameRef}
+          hasError={false}
+        />
+        <CustomInput
+          label={"Phone"}
+          inputType={"number"}
+          defaultValue={currentUserPhone}
+          forwardedRef={phoneRef}
+          hasError={false}
+        />
+        <Button
+          buttonText={"Update Profile"}
+          additionalClasses={"my-6 w-full"}
+        />
+      </form>
       <Modal
         showModal={showModal}
         handleModalClose={handleModalClose}
