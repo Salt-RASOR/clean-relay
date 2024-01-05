09f688a3a4296691928d71462c6b1d72a6169da2
Antti Jankeri <antti.jankeri@gmail.com>
2024-01-05 18:50:26 +0100

    fix: attempt to fix swagger deployment with json magic

--- app/components/Api/react-swagger.tsx
+++ app/components/Api/react-swagger.tsx
@@ -1,13 +1,17 @@
 "use client";
-import React from "react";
 import "swagger-ui-react/swagger-ui.css";
 import SwaggerUI from "swagger-ui-react";
 
 type reactSwaggerProps = {
   spec: Record<string, any>;
+  url?: string;
 };
-const reactSwagger = ({ spec }: reactSwaggerProps) => {
-  return <SwaggerUI spec={spec} />;
+const reactSwagger = ({ spec, url }: reactSwaggerProps) => {
+  if (process.env.NODE_ENV === "development") {
+    return <SwaggerUI spec={spec} />;
+  } else {
+    return <SwaggerUI url={url} />;
+  }
 };
 
 export default reactSwagger;
